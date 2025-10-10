3dd82b8e94665246d9df13abf78d7dff1efd7ebb
Alexander Järsntröm <alexander.jarnstrom@gmail.com>
2025-10-11 01:03:38 +0200

    added -o3

--- pearson/scripts/tests.sh
+++ pearson/scripts/tests.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 FILES=(128.data 256.data 512.data 1024.data)
-#FILES=(128.data)
-DDIR=../data
-ODIR=../data_o
-BIN=../bin/pearson
+#FILES=(1024.data)
+DDIR=data
+ODIR=data_o
+BIN=./pearson
+PBIN=./pearson_par
 
 for f in "${FILES[@]}"; do
-  echo "Running test on ${DDIR}/${f} ... "
-  perf stat --repeat 10 -ddd ./${BIN} ${DDIR}/${f} ${ODIR}/seq_${f}
-  perf stat --repeat 10 -M tma_bad_speculation_group ./${BIN} ${DDIR}/${f} ${ODIR}/seq_${f}
-  perf stat --repeat 10 -M tma_backend_bound_group ./${BIN} ${DDIR}/${f} ${ODIR}/seq_${f}
+  echo "Running threaded tests ... "
+  for t in {0..5}; do
+    thread=$((2 ** t))
+    perf stat --repeat 10 -d ./${PBIN} ${DDIR}/${f} ${ODIR}/seq_${f} ${thread}
+  done
 done
