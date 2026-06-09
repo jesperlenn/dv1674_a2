3dfb27c4a8283980d23ebbfd61fbbe0072861777
Jesper Lennehag <jesper@lennehag.com>
2026-06-09 04:02:48 +0200

    blurred par done

--- blur/par_verify.sh
+++ blur/par_verify.sh
@@ -10,15 +10,15 @@ for thread in 1 2 4 8 16 32
 do
     for image in im1 im2 im3 im4
     do
-        ./blur_par 15 "data/$image.ppm" "./data_o/blur_${image}_par.ppm" $thread
+        ./blur_par 15 "data/$image.ppm" "./data_o/blur_${image}_blurred.ppm" $thread
 
-        if ! cmp -s "./data_o/${image}.ppm" "./data_o/blur_${image}_par.ppm"
+        if ! cmp -s "./data_o/${image}_verified.ppm" "./data_o/blur_${image}_blurred.ppm"
         then
             echo "${red}Error: Incongruent output data detected when blurring image $image.ppm with $thread thread(s)${reset}"
             status=1
         fi
 
-        # rm "./data_o/blur_${image}_par.ppm"
+        rm "./data_o/blur_${image}_blurred.ppm"
     done
 done
 
